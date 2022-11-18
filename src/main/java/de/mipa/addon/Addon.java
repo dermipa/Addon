69d40d109f0a091efa5c6549e266f7a40b2bde2b
dermipa <mipa2086@gmail.com>
2022-11-18 23:37:27 +0100

    Initial commit

---
+++ src/main/java/de/mipa/addon/Addon.java
@@ -0,0 +1,17 @@
+package de.mipa.addon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Addon extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
