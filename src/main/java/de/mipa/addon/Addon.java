beda478cbd9371bb4e1678574423100f9a07d5f1
dermipa <mipa2086@gmail.com>
2022-11-19 01:53:41 +0100

    version: 1.1

--- src/main/java/de/mipa/addon/Addon.java
+++ src/main/java/de/mipa/addon/Addon.java
@@ -1,17 +1,32 @@
 package de.mipa.addon;
 
+import de.mipa.addon.commands.HelpC;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Addon extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getCommand("hilfe").setExecutor(new HelpC());
+        getCommand("help").setExecutor(new HelpC());
+        getCommand("?").setExecutor(new HelpC());
+
+        ConsoleCommandSender console = Bukkit.getConsoleSender();
+        console.sendMessage(ChatColor.DARK_GREEN + "Addon erfolgreich geladen!");
+
+
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
+        ConsoleCommandSender console = Bukkit.getConsoleSender();
+        console.sendMessage(ChatColor.DARK_RED + "Lobbysystem erfolgreich gestoppt!");
+
+
     }
 }
---
+++ src/main/java/de/mipa/addon/commands/HelpC.java
@@ -0,0 +1,17 @@
+package de.mipa.addon.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class HelpC implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        String messageHelp  = "§cMineVillage.de §8» §aWenn du hilfe brauchst nutze /ticket oder joine unserem Discord!";
+
+        sender.sendMessage(messageHelp);
+        return false;
+    }
+}
